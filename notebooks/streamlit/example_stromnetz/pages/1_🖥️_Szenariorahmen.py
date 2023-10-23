c9816871999ee577f19bdb6893b9485b5cc8ea3b
Christophe Cauet <ccauet@point-8.de>
2023-10-23 15:10:24 +0200

    Add copyright notice

--- notebooks/streamlit/example_stromnetz/pages/1_🖥️_Szenariorahmen.py
+++ notebooks/streamlit/example_stromnetz/pages/1_🖥️_Szenariorahmen.py
@@ -1,3 +1,6 @@
+# This code is licensed under a [Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)](https://creativecommons.org/licenses/by-nc-sa/4.0/).
+# Copyright © [Point 8 GmbH](https://point-8.de)
+
 from turtle import color
 import streamlit as st
 import pandas as pd
