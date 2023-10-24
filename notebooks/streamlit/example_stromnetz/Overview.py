18f0c2578e8327c7be5e8a204a6d4d638e23f3c5
sdungs <sdungs@point-8.de>
2023-10-24 15:03:08 +0200

    update streamlit example

--- notebooks/streamlit/example_stromnetz/Overview.py
+++ notebooks/streamlit/example_stromnetz/Overview.py
@@ -1,27 +1,17 @@
-# This code is licensed under a [Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)](https://creativecommons.org/licenses/by-nc-sa/4.0/).
-# Copyright © [Point 8 GmbH](https://point-8.de)
-import base64
-import io
-import pathlib
-
 import streamlit as st
+import io
 from PIL import Image
-
-st.set_page_config(
-    page_title="Overview",
-    # page_icon="",
-)
-
+import base64
 
 def add_logo():
-    file = open(pathlib.Path(__file__).parent / "static/logo.png", "rb")
+    file = open("./static/neps.png", "rb")
     contents = file.read()
     img_str = base64.b64encode(contents).decode("utf-8")
     buffer = io.BytesIO()
     file.close()
     img_data = base64.b64decode(img_str)
     img = Image.open(io.BytesIO(img_data))
-    resized_img = img.resize((160, 84))  # x, y #470,248
+    resized_img = img.resize((180, 80))  # x, y #450,200 
     resized_img.save(buffer, format="PNG")
     img_b64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
 
@@ -42,6 +32,5 @@ def add_logo():
 
 add_logo()
 
-st.title("Regelzonen")
-image = Image.open(pathlib.Path(__file__).parent / "static/regelzonen.png")
+image = Image.open('static/energy.png')
 st.image(image)
--- notebooks/streamlit/example_stromnetz/pages/2_🔮_Marktsimulation.py
+++ notebooks/streamlit/example_stromnetz/pages/2_🔮_Marktsimulation.py
@@ -1,12 +1,6 @@
-# This code is licensed under a [Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)](https://creativecommons.org/licenses/by-nc-sa/4.0/).
-# Copyright © [Point 8 GmbH](https://point-8.de)
-
-import pandas as pd
-import plotly.express as px
 import streamlit as st
+
 from Overview import add_logo
-from PIL import Image
-from plotly.colors import n_colors
 
 add_logo()
 
